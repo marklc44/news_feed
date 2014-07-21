68a3303325048f27644a6c91530dd21ba67c2362
Mark Centoni <marklc44@gmail.com>
2014-07-21 16:24:17 -0700

    server running

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
 var express = require('express'),
 		ejs = require('ejs'),
 		bodyParser = require('body-parser');
+
+var app = express();
+
+// Templating
+//app.engine();
+app.set('view engine', 'ejs');
+
+
+// Routes
+app.get('/articles', function(req, res) {
+
+});
+
+app.get('/articles/new', function(req, res) {
+	
+});
+
+app.post('/articles', function(req, res) {
+	
+});
+
+app.get('/articles/:id', function(req, res) {
+	
+});
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/about', function(req, res) {
+	
+});
+
+app.get('/contact', function(req, res) {
+	
+});
+
+app.listen(3000);
+
+

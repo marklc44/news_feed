c82666eafe35ffbc32cc5d7238fbd90cc2bb85a4
Mark Centoni <marklc44@gmail.com>
2014-07-21 16:59:49 -0700

    set up articles listing and new article form

--- app.js
+++ app.js
@@ -5,37 +5,46 @@ var express = require('express'),
 var app = express();
 
 // Templating
-//app.engine();
+// app.engine('html', ejs.__engine);
 app.set('view engine', 'ejs');
 
+// Data
+var articles = [
+	{
+		title: "Sample",
+		author: "Sample Author",
+		description: "Sample Description"
+	}
+];
 
 // Routes
-app.get('/articles', function(req, res) {
 
+app.get('/articles', function(req, res) {
+	res.render('articles/articles', {articles: articles});
 });
 
 app.get('/articles/new', function(req, res) {
-	
+	res.render('articles/new');
 });
 
 app.post('/articles', function(req, res) {
-	
+
 });
 
 app.get('/articles/:id', function(req, res) {
-	
+	res.render('articles/show');
 });
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('site/index');
 });
 
 app.get('/about', function(req, res) {
-	
+	res.render('site/about');
 });
 
 app.get('/contact', function(req, res) {
-	
+	res.render('site/contact');
 });
 
 app.listen(3000);
